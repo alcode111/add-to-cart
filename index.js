cd470a54387ced25e2f6438d74502ba86cf97ca5
Ismail <141173099+alcode111@users.noreply.github.com>
2024-01-02 18:34:14 +0100

    Retrieve HTML elements to establish the DOM structure

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ const appSettings = {
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingItemsInDB = ref(database, "shoppingItems")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButton = document.getElementById("add-button")
+const shoppingItemsUlEl = document.getElementById("shopping-items")
